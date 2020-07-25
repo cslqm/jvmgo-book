65d46726bf9e41b232c5b4a9e587e9ae3c089bb7
cslqm <qiangmin2016@outlook.com>
2020-07-25 09:11:11 +0800

    jvmgo: ch03
    Support find and parse class file.
    
    Signed-off-by: cslqm <qiangmin2016@outlook.com>

---
+++ ch03/classfile/member_info.go
@@ -0,0 +1,57 @@
+package classfile
+
+const (
+	CONSTANT_Class              = 7
+	CONSTANT_Fieldref           = 9
+	CONSTANT_Methodref          = 10
+	CONSTANT_InterfaceMethodref = 11
+	CONSTANT_String             = 8
+	CONSTANT_Integer            = 3
+	CONSTANT_Float              = 4
+	CONSTANT_Long               = 5
+	CONSTANT_Double             = 6
+	CONSTANT_NameAndType        = 12
+	CONSTANT_Utf8               = 1
+	CONSTANT_MethodHandle       = 15
+	CONSTANT_MethodType         = 16
+	CONSTANT_InvokeDynamic      = 18
+)
+
+type MemberInfo struct {
+	cp              ConstantPool
+	accessFlags     uint16
+	nameIndex       uint16
+	descriptorIndex uint16
+	attributes      []AttributeInfo
+}
+
+func readMembers(reader *ClassReader, cp ConstantPool) []*MemberInfo {
+	memberCount := reader.readUint16()
+	members := make([]*MemberInfo, memberCount)
+	for i := range members {
+		members[i] = readMember(reader, cp)
+	}
+	return members
+}
+
+func readMember(reader *ClassReader, cp ConstantPool) *MemberInfo {
+	return &MemberInfo{
+		cp:              cp,
+		accessFlags:     reader.readUint16(),
+		nameIndex:       reader.readUint16(),
+		descriptorIndex: reader.readUint16(),
+		attributes:      readAttributes(reader, cp),
+	}
+}
+
+func (self *MemberInfo) AccessFlags() uint16 {
+	return self.accessFlags
+}
+
+func (self *MemberInfo) Name() string {
+	return self.cp.getUtf8(self.nameIndex)
+}
+
+func (self *MemberInfo) Descriptor() string {
+	return self.cp.getUtf8(self.descriptorIndex)
+}
